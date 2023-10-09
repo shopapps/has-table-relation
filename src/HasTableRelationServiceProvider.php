f863cbdf3deedd9335e22f718e02b2acfe0f5107
Paul <paul@shopapps.co.uk>
2023-10-09 07:59:49 +0100

    Create HasTableRelationServiceProvider.php
    first commit

---
+++ src/HasTableRelationServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ShopApps\HasTableRelation;
+
+use Illuminate\Support\ServiceProvider;
+
+class HasTableRelationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // You can load migrations, views, or routes here if needed in the future
+    }
+
+    public function register()
+    {
+        // Register any bindings or singletons here if needed in the future
+    }
+}
